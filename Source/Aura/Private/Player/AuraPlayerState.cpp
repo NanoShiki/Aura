6913ff875f1b0b8441eace48d06c32150c7ec1d1
NanoShiki <2453788828@qq.com>
2025-07-12 23:16:53 +0800

    Introduce GAS (ASC and AS)

---
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -0,0 +1,24 @@
+// Copyright Nanoshiki
+
+
+#include "Player/AuraPlayerState.h"
+
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+
+AAuraPlayerState::AAuraPlayerState() {
+	AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent->SetIsReplicated(true);
+	
+	AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("AttributeSet");
+	
+	SetNetUpdateFrequency(100.f);	//服务器尝试更新客户端的频率
+}
+
+UAbilitySystemComponent* AAuraPlayerState::GetAbilitySystemComponent() const {
+	return AbilitySystemComponent;
+}
+
+UAttributeSet* AAuraPlayerState::GetAttributeSet() const {
+	return AttributeSet;
+}
