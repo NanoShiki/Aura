76c05c6696f30f753a2d24631a6bef1981b9d21d
NanoShiki <2453788828@qq.com>
2025-07-19 22:24:26 +0800

    Broadcast Pickup GameplayTag

--- Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -11,9 +11,6 @@ void UAuraAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* Ability
 	const FGameplayEffectSpec& EffectSpec, FActiveGameplayEffectHandle EffectHandle) {
 	FGameplayTagContainer TagContainer;
 	EffectSpec.GetAllAssetTags(TagContainer);
-	for (const auto& Tag : TagContainer) {
-		//TODO: Broadcast the tag to the Widget Controller
-		const FString Msg = FString::Printf(TEXT("GE Tag: %s"), *Tag.ToString());
-		GEngine->AddOnScreenDebugMessage(-1, 8.f, FColor::Blue, Msg);
-	}
+
+	EffectAssetTags.Broadcast(TagContainer);
 }
